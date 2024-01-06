530cb4b87b0c15dbecf53e97dcc4130e2e8746c9
Magdiel-GVdz <magdiel.gvdz@gmail.com>
2024-01-06 17:50:34 -0600

    add main

---
+++ main.tf
@@ -0,0 +1,48 @@
+resource "aws_instance" "mi_primera_chamba" {
+  ami             = var.ami
+  instance_type   = var.instance-type
+  key_name        = var.key-pair
+  security_groups = [aws_security_group.TF_SG.name]
+}
+
+resource "aws_security_group" "TF_SG" {
+  name        = "mi primer security group"
+  description = "mi primer security group"
+  vpc_id      = var.vpc
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
